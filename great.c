9a84ee927b3ae4e5ed8d2700645906df813f348a
sridhar1727 <sridharock1112@gmail.com>
2016-09-09 19:24:27 +0530

    Update and rename README.md to great.c

---
+++ great.c
@@ -0,0 +1,27 @@
+# sri
+#include <stdio.h>
+int main()
+{
+  int a[500],n,i,j,t,k;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		scanf("%d",&a[i]);
+	scanf("%d",&k);
+	for(i=1;i<=n;i++)
+	{
+		for(j=i+1;j<=n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	for(i=1;i<=k;i++)
+	{
+		printf("%d",a[i]);
+	}
+	return 0;
+}
